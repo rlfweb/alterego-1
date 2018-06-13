0377bdc280b318844457dc5cd35ccf1a561062a5
Lawrence Gosset <lawrencegosset@gmail.com>
2018-06-13 05:58:06 +0100

    minor tweaks

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -17,9 +17,9 @@
  */
 function alterego_woocommerce_setup() {
 	add_theme_support( 'woocommerce' );
-	// add_theme_support( 'wc-product-gallery-zoom' );
-	// add_theme_support( 'wc-product-gallery-lightbox' );
-	// add_theme_support( 'wc-product-gallery-slider' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 'alterego_woocommerce_setup' );
 
